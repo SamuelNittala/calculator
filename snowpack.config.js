cb655bd88388903885bc3152b3adedcfbd3bcfc5
SamuelNittala <nittalasamuel7@gmail.com>
2022-04-16 18:32:37 +0530

    remove emotion, add styled components and hmr

--- snowpack.config.js
+++ snowpack.config.js
@@ -1,4 +1,9 @@
 module.exports = {
+  mount: {
+    public: { url: '/', static: true },
+    src: { url: '/dist' },
+  },
+  plugins: ['@snowpack/plugin-react-refresh'],
   extends: '@snowpack/app-scripts-react',
   plugins: [],
 };
