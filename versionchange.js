d22a5b5ebe1514b0ccb9be9ec663d4334aab5b9c
SamuelNittala <nittalasamuel7@gmail.com>
2022-10-05 08:09:49 +0530

    integrate version change on main branch only

--- versionchange.js
+++ versionchange.js
@@ -1,16 +1,23 @@
 const path = require('path');
 const fs = require('fs');
+const gitBranchIs = require('git-branch-is');
 
-const currentVersionJsonPath = path.join(__dirname, 'public', 'currentversion.json');
+gitBranchIs('main')
+  .then(() => {
+    const currentVersionJsonPath = path.join(__dirname, 'public', 'currentversion.json');
 
-const currentVersion = JSON.parse(
-  fs.readFileSync(currentVersionJsonPath, {
-    encoding: 'utf-8',
-    flag: 'r',
-  }),
-).version;
+    const currentVersion = JSON.parse(
+      fs.readFileSync(currentVersionJsonPath, {
+        encoding: 'utf-8',
+        flag: 'r',
+      }),
+    ).version;
 
-fs.writeFileSync(currentVersionJsonPath, `{"version": ${currentVersion + 0.01}}`, {
-  encoding: 'utf-8',
-  flag: 'w',
-});
+    fs.writeFileSync(currentVersionJsonPath, `{"version": ${currentVersion + 0.01}}`, {
+      encoding: 'utf-8',
+      flag: 'w',
+    });
+  })
+  .catch((err) => {
+    console.err(err);
+  });
